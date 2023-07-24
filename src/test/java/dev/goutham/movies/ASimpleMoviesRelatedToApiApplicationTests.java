f0784b7386a969ac2546f86eb0f1ca87dadfc74a
GouthamMenneni <mennenigoutham123@gmail.com>
2023-07-23 19:22:03 -0500

    first commit

---
+++ src/test/java/dev/goutham/movies/ASimpleMoviesRelatedToApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.goutham.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASimpleMoviesRelatedToApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
